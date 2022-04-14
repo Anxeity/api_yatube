f16477213fd252faafdd38ca6531fdd10de3ba8a
Михаил Глазов <glazov_52@mail.ru>
2022-04-14 20:55:45 +0300

    FirstReview

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.authtoken import views
-from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('admin/', admin.site.urls),
